41327ed6cf848063fcae3a6264dd28102fffad62
liquidcarbon <akscrps@gmail.com>
2024-08-15 11:16:28 -0600

    debug PATH

--- pup.sh
+++ pup.sh
@@ -88,10 +88,7 @@ get_pup
 get_pixi() {
   if ! command -v pixi &> /dev/null; then
     curl -fsSL $PIXI_INSTALL_URL | bash
-    echo hi $(ls -l ~/.bashrc)
-    source "$HOME/.bashrc"  # for GHA
-    echo $PATH
-    pixi -V
+    export PATH=$HOME/.pixi/bin:$PATH  # for GHA
     echo "✨ $(pixi -V) installed"
   else
     echo "✨ $(pixi -V) found"
