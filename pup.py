5bf7c66c20c04ec18abbf68ac39f9a642ccedf2e
liquidcarbon <akscrps@gmail.com>
2024-10-03 21:26:10 -0600

    pup hi and find_home via woof.log

--- pup.py
+++ pup.py
@@ -4,30 +4,51 @@ The CLI for pup, a cute python project manager.
 """
 
 import sys
+from pathlib import Path
 
 import click
 
 
+class PupException(Exception):
+    pass
+
+
 class Pup:
-    FILE: str = __file__
+    FILE: Path = Path(__file__)
     LOG_TIME_FORMAT: str = "%Y-%m-%d %H:%M:%S"
-    PYTHON: str = sys.executable
+    PUP_HOME_MARKER: str = "woof.log"
+    PYTHON: Path = Path(sys.executable)
+    PYTHON_PREFIX: Path = Path(sys.prefix)
 
     @classmethod
-    def pedigree(cls):
-        return f"pedigree: {cls.PYTHON} {cls.FILE}"
+    def find_home(cls, prefix=PYTHON_PREFIX) -> Path:
+        if (prefix / cls.PUP_HOME_MARKER).exists():
+            return prefix
+        elif prefix.parent == prefix:
+            _home = cls.PUP_HOME_MARKER
+            click.echo(f"🐶's {_home} not found in this folder or its parents")
+            exit(1)
+        else:
+            return cls.find_home(prefix.parent)
+
+    @classmethod
+    def pedigree(cls) -> str:
+        return f"🐶 = {cls.PYTHON} {cls.FILE}"
 
 
 @click.group
-def main():
+@click.pass_context
+def main(ctx):
     """Call pup and friends for all your python needs."""
     pass
 
 
 @main.command(name="hi")
 def say_hi():
-    """Say hi to pup"""
-    click.echo("🐶 says: woof!")
+    """Say hi to pup."""
+    click.echo(Pup.pedigree())
+    click.echo(f"🐶 home is: {Pup.find_home()}")
+    click.echo("🐶 says: woof! check woof.log for pup command history")
 
 
 if __name__ == "__main__":
